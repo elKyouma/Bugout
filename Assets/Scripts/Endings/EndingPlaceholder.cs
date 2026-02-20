bd7de8789da1f715603f2641d1b35ee5499a7bf2
Adrian Gliźniewicz <adrian2402glizniewicz@gmail.com>
2026-02-20 15:35:08 +0100

    Better tween addon

--- Assets/Scripts/Endings/EndingPlaceholder.cs
+++ Assets/Scripts/Endings/EndingPlaceholder.cs
@@ -1,8 +1,6 @@
-using System.Collections;
-using System.Collections.Generic;
+using PrimeTween;
 using UnityEngine;
 using UnityEngine.UI;
-using Utility;
 
 public class EndingPlaceholder : MonoBehaviour
 {
@@ -10,17 +8,12 @@ public class EndingPlaceholder : MonoBehaviour
 
     private void Awake()
     {
-        transform.localScale = Vector3.zero;
-        if ((Status)PlayerPrefs.GetInt(ending.name, 0) == Status.Completed && ending.icon)
-        {
-            GetComponent<Image>().sprite = ending.icon;
+        GetComponent<Image>().sprite = ending.icon;
 
-            if(PlayerPrefs.GetString("CurrendEnding") == ending.name)
-                transform.LeanScale(Vector3.one * 0.5f, 0.5f).setEaseInOutSine().setOnComplete(() => { transform.LeanScale(Vector3.one * 1.5f, 1f).setEaseInOutSine().setLoopPingPong(); });//.setLoopPingPong();
-            else
-                transform.LeanScale(Vector3.one, 0.5f).setEaseInOutExpo();
-        }
+        if (PlayerPrefs.GetString("CurrendEnding") == ending.name)
+            Tween.Scale(transform, Vector3.one * 0.5f, 0.5f, Ease.InOutSine)
+                .OnComplete(() => Tween.Scale(transform, Vector3.one * 1.5f, 1f, Ease.InOutSine, -1, CycleMode.Yoyo));
         else
-            transform.LeanScale(Vector3.one, 0.5f).setEaseInOutExpo();//.setLoopPingPong();
+            Tween.Scale(transform, Vector3.one, 0.5f, Ease.InOutExpo);
     }
 }
